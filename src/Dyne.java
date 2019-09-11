83ba739faabec3dad2fcf3c1a5ffaa0499452c45
yokarak <ande67f3@edu.easj.dk>
2019-09-11 12:33:30 +0200

    smart dyne. - privat variabler - tilføjelse af optimal tempt - tilføjelse af brændfare - tilføjelse af omryst dyne pga. over temperaturAVG

--- src/Dyne.java
+++ src/Dyne.java
@@ -6,28 +6,44 @@ public class Dyne {
     double locationTempt; // specifikt temp. et sted
     double temperaturAVG; //temperatur over hele dynen, gennemsnitlig
 
-    double minimunTempt = 18; //a+=18
-    double maximunTempt = 30; //a2-=30
-    boolean mereVarme; // om der skal mere varme til
-    boolean mindreVarme; // om der skal mindre varme til
+    private double minimunTempt = 18; //a+=18
+    private double maximunTempt = 30; //a2-=30
+    private boolean mereVarme; // om der skal mere varme til
+    private boolean mindreVarme; // om der skal mindre varme til
+    private boolean optimalTempt;
 
     // metoder
     void indstilDyne() {
-        if (locationTempt < minimunTempt) {
+        if (locationTempt < minimunTempt) { // funktion
             mereVarme = true;
-        }
-        if (locationTempt > maximunTempt) {
+        } else if (locationTempt > maximunTempt) {
             mindreVarme = true;
-
         }
-        if (mindreVarme == true) {
+
+        if (mindreVarme) {
             System.out.println("mindre Varme under dynen");
-        }
-
-        if (mereVarme == true) {
+        } else if (mereVarme) {
             System.out.println("mere Varme under dynen");
         }
-    }
+
+        if (!mereVarme && !mindreVarme) {
+            System.out.println("Optimal Temperatur");
+        }
+
+        if (temperaturAVG > 45) {
+            System.out.println("Omryst dyne for optimal tempt");
+        }
+
+        if (locationTempt > 80) {
+            System.out.println("!!Alarm Brændfare!!");
+        }
+/*
+        if () { //mellemrum for pænere udskrift
+            System.out.print("  ");
+        }
+*/
+
+}
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,33 @@ public class Main {
     public static void main(String[] args) {
 
         Dyne projekt1 = new Dyne(); //skabelsen af projekt1 (hænger det sammen med variabler fra Dyne)
+        Dyne projekt2 = new Dyne();
+        Dyne projekt3 = new Dyne();
+        Dyne projekt4 = new Dyne();
 
-
+        System.out.println("Projekt 1");
         projekt1.temperaturAVG = 20;
-        projekt1.locationTempt = 100;
+        projekt1.locationTempt = 25;
         projekt1.indstilDyne();
+
+        System.out.println("Projekt 2");
+        projekt2.temperaturAVG = 50;
+        projekt2.locationTempt = 15;
+        projekt2.indstilDyne();
+
+        System.out.println("Projekt 3");
+        projekt3.temperaturAVG = 15;
+        projekt3.locationTempt = 30;
+        projekt3.indstilDyne();
+
+        System.out.println("Projekt 4");
+        projekt4.temperaturAVG =20;
+        projekt4.locationTempt =100;
+        projekt4.indstilDyne();
+
+
+
+
 
 
     }
